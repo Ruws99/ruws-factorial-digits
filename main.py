abf2882436af85d86c379abd7f03d9fabe00d42a
Ruws99 <79838587+Ruws99@users.noreply.github.com>
2022-02-23 12:38:10 +0200

    working directory uploaded

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+import numpy as np
+
+# Read argument passed in docker run command
+number=sys.argv[1]
+
+# Test whether argument is number
+if number.isdigit():
+    # cast to int for factorial
+    number=int(number)
+
+    # Compute factorial
+    fact=np.math.factorial(number)
+
+    total=0
+    while fact>0:
+        total=total+np.mod(fact,10)     # Shift right digits through mod 10
+        fact=fact//10   # Get rid of remainders
+
+    print(round(total))
+    
+else:
+    print("Enter a positive integer")
+
